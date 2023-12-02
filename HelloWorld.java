0ef440e3a3dcd72a0489cef0066e4256a1485412
christopherjneelankavil <christopherneelankavil@gmail.com>
2023-12-02 22:30:16 +0530

    Added Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+//Output
+//Hello World!
+
+
+// Algorithm for HelloWorld Program
+
+// Step 1: Start
+
+// Step 2: Initialize the main method
+//     2.1: Declare the main method with the signature: public static void main(String[] args)
+
+// Step 3: Print "Hello World!"
+//     3.1: Use the System.out.println() method to print the string "Hello World!" to the console
+
+// Step 4: End
+

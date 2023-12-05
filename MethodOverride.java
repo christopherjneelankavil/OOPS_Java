7b3da9281011ad9c9c627f630e5099f571374e71
Alan Jose Santo <87328737+realityaudiostudio@users.noreply.github.com>
2023-12-05 18:41:30 +0530

    output changed

--- MethodOverride.java
+++ MethodOverride.java
@@ -37,3 +37,23 @@ public class EmpOver {
 Name of the class is Employee
 Salary of employee is : 10000.0
 The Salary of engineer is  : 20000.0*/
+// Algorithm for Method Override
+
+// Step 1: Define the Employee class
+//     1.1: Declare class Employee
+//     1.2: Declare method `display()` to print "Name of the class employee"
+//     1.3: Declare method `calcSalary()` to print "Salary of the employee is 10000"
+
+// Step 2: Define the Engineer class
+//     2.1: Declare class Engineer extending Employee
+//     2.2: Override the `calcSalary()` method in the Engineer class to print "Salary of the engineer is 20000"
+
+// Step 3: Define the MethodOverride class
+//     3.1: Declare a public class MethodOverride
+//     3.2: Inside the main method:
+//         3.2.1: Create an instance of Employee and assign it a reference to an Engineer object (Polymorphism)
+//         3.2.2: Call the `display()` method on the instance, which will print "Name of the class employee"
+//         3.2.3: Call the `calcSalary()` method on the instance, which will invoke the overridden method in Engineer
+//             3.2.3.1: The overridden method in Engineer prints "Salary of the engineer is 20000"
+
+// Step 4: End
